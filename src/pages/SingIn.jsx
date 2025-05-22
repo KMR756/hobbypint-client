3bc487411decca74bba496b5d922b47cda47e225
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-05-22 11:22:12 +0600

    design signin and signup page with different device responsiveness

--- src/pages/SingIn.jsx
+++ src/pages/SingIn.jsx
@@ -1,7 +1,116 @@
 import React from "react";
+import singIn from "/sign-in.svg";
+import logo from "../assets/logo-white.png";
+const SingUp = () => {
+  return (
+    <>
+      <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+        <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+          <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+            <div>
+              <img src={logo} class="w-32 mx-auto" />
+            </div>
+            <div class="mt-12 flex flex-col items-center">
+              <h1 class="text-2xl xl:text-3xl font-extrabold">Sign In now!</h1>
+              <div class="w-full flex-1 mt-8">
+                <div class="flex flex-col items-center">
+                  <button class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                    <div class="bg-white p-2 rounded-full">
+                      <svg class="w-4" viewBox="0 0 533.5 544.3">
+                        <path
+                          d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                          fill="#4285f4"
+                        />
+                        <path
+                          d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                          fill="#34a853"
+                        />
+                        <path
+                          d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                          fill="#fbbc04"
+                        />
+                        <path
+                          d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                          fill="#ea4335"
+                        />
+                      </svg>
+                    </div>
+                    <span class="ml-4">Google</span>
+                  </button>
+                </div>
 
-const SingIn = () => {
-  return <div>SingIn</div>;
+                <div class="my-12 border-b text-center">
+                  <div class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                    Or Sing In with e-mail
+                  </div>
+                </div>
+
+                <div class="mx-auto max-w-xs">
+                  <input
+                    class="mt-5 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="email"
+                    placeholder="Email"
+                  />
+
+                  <input
+                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <button class="mt-5 tracking-wide font-semibold bg-[#c30e59d0] text-gray-100 w-full py-4 rounded-lg hover:bg-[#C30E59] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                    <svg
+                      class="w-8 h-5 -ml-2"
+                      fill="#ffffff"
+                      viewBox="0 0 16 16"
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                    >
+                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <rect
+                          width="16"
+                          height="16"
+                          id="icon-bound"
+                          fill="none"
+                        ></rect>{" "}
+                        <path d="M14,14l0,-12l-6,0l0,-2l8,0l0,16l-8,0l0,-2l6,0Zm-6.998,-0.998l4.998,-5.002l-5,-5l-1.416,1.416l2.588,2.584l-8.172,0l0,2l8.172,0l-2.586,2.586l1.416,1.416Z"></path>{" "}
+                      </g>
+                    </svg>
+
+                    <span class="ml-3">Sign In</span>
+                  </button>
+
+                  <p class="mt-6  text-xs text-gray-600 text-center">
+                    not yet registered?{" "}
+                    <span>
+                      <a
+                        href="#"
+                        class="border-b  text-xs  border-gray-500 border-dotted"
+                      >
+                        Registration
+                      </a>
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
+            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat">
+              <img src={singIn} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default SingIn;
+export default SingUp;
