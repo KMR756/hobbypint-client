5f7bfd2f183493922cc15668ccb4852c019dbf61
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-05-23 05:33:18 +0600

    adding group deleting funtionality

--- src/components/MyGroupCard.jsx
+++ src/components/MyGroupCard.jsx
@@ -1,9 +1,77 @@
-import React from "react";
+import Swal from "sweetalert2";
 
-const myGroupCard = ({ myGroup }) => {
-  console.log(myGroup);
+const MyGroupCard = ({ groups, myGroups, setMyGroups }) => {
+  console.log(groups);
 
-  return <div>myGroupCard</div>;
+  const {
+    _id,
+    groupName,
+    groupCategory,
+
+    startDate,
+    location,
+    photo,
+  } = groups;
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/groups/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              if (data.deletedCount) {
+                // ✅ Update the state to remove deleted group
+                setMyGroups(myGroups.filter((g) => g._id !== _id));
+              }
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your group has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+  return (
+    <tr>
+      <td>
+        <div className="flex items-center  gap-3">
+          <div className="avatar">
+            <div className="mask mask-squircle h-12 w-12">
+              <img src={photo} alt="Avatar Tailwind CSS Component" />
+            </div>
+          </div>
+          <div>
+            <div className="font-bold">{groupName}</div>
+            <div className="text-sm opacity-50">{location}</div>
+          </div>
+        </div>
+      </td>
+      <td>{groupCategory}</td>
+      <td>{startDate}</td>
+      <th className="space-y-3 lg:space-y-0 lg:space-x-3">
+        <button className="btn btn-primary btn-xs">Edit</button>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn btn-primary btn-xs"
+        >
+          Delete
+        </button>
+      </th>
+    </tr>
+  );
 };
 
-export default myGroupCard;
+export default MyGroupCard;
--- src/pages/MyGroup.jsx
+++ src/pages/MyGroup.jsx
@@ -1,77 +1,46 @@
 import { getAuth } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router";
+import MyGroupCard from "../components/myGroupCard";
 
 const MyGroup = () => {
   const auth = getAuth();
   const userEmail = auth.currentUser.email;
+  const AllGroups = useLoaderData();
   // console.log(userEmail);
+  const [myGroups, setMyGroups] = useState(
+    AllGroups.filter((group) => group.email === userEmail)
+  );
 
-  const groups = useLoaderData();
   // console.log(groups);
-
-  const myGroups = groups.filter((group) => group.email === userEmail);
-  console.log(myGroups);
 
   return (
     <>
       <div>
         <div className="w-[98%] lg:w-8/12 mx-auto my-20">
-          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="lato-regular w-full text-xl text-left rtl:text-right text-gray-500 ">
-              <thead class="text-xl lato-bold text-white uppercase bg-[#C30E59] ">
+          <div className="overflow-x-auto">
+            <table className="table bg-white">
+              {/* head */}
+              <thead>
                 <tr>
-                  <th scope="col" class="px-6 py-3">
-                    Group name
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Group category
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Maximum member
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Starting Date
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Action
-                  </th>
+                  <th className="text-center">Group Name</th>
+                  <th>Category</th>
+                  <th>Starting Date:</th>
+                  <th>Action</th>
                 </tr>
               </thead>
-              {myGroups.map((myGroup) => (
-                <tbody>
-                  <tr class="odd:bg-white  even:bg-gray-50  border-b  border-gray-200">
-                    <th
-                      scope="row"
-                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                    >
-                      {myGroup.groupName}
-                    </th>
-                    <td class="px-6 lg:pl-20 pl-10 py-6">
-                      {myGroup.groupCategory}
-                    </td>
-                    <td class="px-6 pl-11 lg:pl-23 py-6">
-                      {myGroup.maxMember}
-                    </td>
-                    <td class="px-6 pl-5 lg:pl-13 py-6">{myGroup.startDate}</td>
-
-                    <td class="px-6 py-4 space-x-4">
-                      <a
-                        href="#"
-                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </a>
-                      <a
-                        href="#"
-                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Delete
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              ))}
+              <tbody>
+                {/* row 1 */}
+                {myGroups.map((groups) => (
+                  <MyGroupCard
+                    groups={groups}
+                    key={groups._id}
+                    setMyGroups={setMyGroups}
+                    myGroups={myGroups}
+                  ></MyGroupCard>
+                ))}
+              </tbody>
+              {/* foot */}
             </table>
           </div>
         </div>
